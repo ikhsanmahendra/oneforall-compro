c60100e1be89cc82d44f29ac37cdad15896e1e62
Ikhsan <ikhsanmahendra14@gmail.com>
2024-12-18 09:39:53 +0700

    fix: home

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -4,9 +4,21 @@
 
 @section('content')
 <div class="bg-primary-500 min-h-screen">
+    <!-- Background Icons -->
+    <div
+        class="absolute inset-0 flex flex-wrap justify-between items-start opacity-80 py-14 md:py-32 lg:py-48 pointer-events-none">
+        <img src="{{ asset('assets/icons/burger.png') }}" alt="burger" class="w-16 md:w-24 lg:w-28">
+        <img src="{{ asset('assets/icons/kentang.png') }}" alt="kentang" class="w-16 md:w-24 lg:w-28">
+        <img src="{{ asset('assets/icons/minum.png') }}" alt="minum" class="w-16 md:w-24 lg:w-28">
+        <img src="{{ asset('assets/icons/mie.png') }}" alt="mie" class="w-16 md:w-24 lg:w-28">
+    </div>
     <div class=" py-3 px-8 ">
+
+
+
+
         <!-- title text -->
-        <div class="pt-8 md:pt-28 lg:pt-52 space-y-2 md:space-y-8">
+        <div class="pt-8 md:pt-28 lg:pt-52 space-y-2 md:space-y-8 relative z-10">
             <h1
                 class="text-2xl md:text-4xl lg:text-5xl xl:text-6xl text-white font-sans font-bold text-center  md:px-20 xl:px-52">
                 Nikmati
@@ -28,6 +40,7 @@
                 <x-button class="font-sans text-sm md:text-base font-semibold">Lihat Menu </x-button>
             </div>
         </div>
+
 
         <!-- slider -->
         <div class="my-20">
--- resources/views/partials/slider.blade.php
+++ resources/views/partials/slider.blade.php
@@ -1,74 +1,95 @@
 <div class="swiper mySwiper">
     <div class="swiper-wrapper">
         <!-- TODO: make dynamic from database -->
-
         <div class="swiper-slide  ">
-            <img src="{{ asset('assets/charizard.jpg') }}" alt="Slide 1"
+            <img src="{{ asset('assets/ayamgoreng.jpg') }}" alt="Slide 1"
                 class="w-40 h-52 md:w-52 md:h-60 lg:w-72 lg:h-80 rounded-3xl object-cover">
         </div>
 
         <div class="swiper-slide  ">
-            <img src="{{ asset('assets/charizard.jpg') }}" alt="Slide 1"
+            <img src="{{ asset('assets/ayamgoreng2.jpg') }}" alt="Slide 1"
                 class="w-36 h-52 md:w-40 md:h-60 lg:w-72 lg:h-80 rounded-3xl object-cover">
         </div>
         <div class="swiper-slide  ">
-            <img src="{{ asset('assets/charizard.jpg') }}" alt="Slide 1"
+            <img src="{{ asset('assets/ayamchicken.jpg') }}" alt="Slide 1"
                 class="w-36 h-52 md:w-40 md:h-60 lg:w-72 lg:h-80 rounded-3xl object-cover">
         </div>
         <div class="swiper-slide  ">
-            <img src="{{ asset('assets/charizard.jpg') }}" alt="Slide 1"
+            <img src="{{ asset('assets/ayamgoreng.jpg') }}" alt="Slide 1"
+                class="w-40 h-52 md:w-52 md:h-60 lg:w-72 lg:h-80 rounded-3xl object-cover">
+        </div>
+
+        <div class="swiper-slide  ">
+            <img src="{{ asset('assets/ayamgoreng2.jpg') }}" alt="Slide 1"
                 class="w-36 h-52 md:w-40 md:h-60 lg:w-72 lg:h-80 rounded-3xl object-cover">
         </div>
         <div class="swiper-slide  ">
-            <img src="{{ asset('assets/charizard.jpg') }}" alt="Slide 1"
+            <img src="{{ asset('assets/ayamchicken.jpg') }}" alt="Slide 1"
                 class="w-36 h-52 md:w-40 md:h-60 lg:w-72 lg:h-80 rounded-3xl object-cover">
         </div>
         <div class="swiper-slide  ">
-            <img src="{{ asset('assets/charizard.jpg') }}" alt="Slide 1"
+            <img src="{{ asset('assets/ayamgoreng.jpg') }}" alt="Slide 1"
+                class="w-40 h-52 md:w-52 md:h-60 lg:w-72 lg:h-80 rounded-3xl object-cover">
+        </div>
+
+        <div class="swiper-slide  ">
+            <img src="{{ asset('assets/ayamgoreng2.jpg') }}" alt="Slide 1"
                 class="w-36 h-52 md:w-40 md:h-60 lg:w-72 lg:h-80 rounded-3xl object-cover">
         </div>
         <div class="swiper-slide  ">
-            <img src="{{ asset('assets/charizard.jpg') }}" alt="Slide 1"
+            <img src="{{ asset('assets/ayamchicken.jpg') }}" alt="Slide 1"
                 class="w-36 h-52 md:w-40 md:h-60 lg:w-72 lg:h-80 rounded-3xl object-cover">
         </div>
         <div class="swiper-slide  ">
-            <img src="{{ asset('assets/charizard.jpg') }}" alt="Slide 1"
+            <img src="{{ asset('assets/ayamgoreng.jpg') }}" alt="Slide 1"
+                class="w-40 h-52 md:w-52 md:h-60 lg:w-72 lg:h-80 rounded-3xl object-cover">
+        </div>
+
+        <div class="swiper-slide  ">
+            <img src="{{ asset('assets/ayamgoreng2.jpg') }}" alt="Slide 1"
                 class="w-36 h-52 md:w-40 md:h-60 lg:w-72 lg:h-80 rounded-3xl object-cover">
         </div>
         <div class="swiper-slide  ">
-            <img src="{{ asset('assets/charizard.jpg') }}" alt="Slide 1"
+            <img src="{{ asset('assets/ayamchicken.jpg') }}" alt="Slide 1"
                 class="w-36 h-52 md:w-40 md:h-60 lg:w-72 lg:h-80 rounded-3xl object-cover">
         </div>
         <div class="swiper-slide  ">
-            <img src="{{ asset('assets/charizard.jpg') }}" alt="Slide 1"
+            <img src="{{ asset('assets/ayamgoreng.jpg') }}" alt="Slide 1"
+                class="w-40 h-52 md:w-52 md:h-60 lg:w-72 lg:h-80 rounded-3xl object-cover">
+        </div>
+
+        <div class="swiper-slide  ">
+            <img src="{{ asset('assets/ayamgoreng2.jpg') }}" alt="Slide 1"
                 class="w-36 h-52 md:w-40 md:h-60 lg:w-72 lg:h-80 rounded-3xl object-cover">
         </div>
         <div class="swiper-slide  ">
-            <img src="{{ asset('assets/charizard.jpg') }}" alt="Slide 1"
+            <img src="{{ asset('assets/ayamchicken.jpg') }}" alt="Slide 1"
                 class="w-36 h-52 md:w-40 md:h-60 lg:w-72 lg:h-80 rounded-3xl object-cover">
         </div>
         <div class="swiper-slide  ">
-            <img src="{{ asset('assets/charizard.jpg') }}" alt="Slide 1"
+            <img src="{{ asset('assets/ayamgoreng.jpg') }}" alt="Slide 1"
+                class="w-40 h-52 md:w-52 md:h-60 lg:w-72 lg:h-80 rounded-3xl object-cover">
+        </div>
+
+        <div class="swiper-slide  ">
+            <img src="{{ asset('assets/ayamgoreng2.jpg') }}" alt="Slide 1"
                 class="w-36 h-52 md:w-40 md:h-60 lg:w-72 lg:h-80 rounded-3xl object-cover">
         </div>
         <div class="swiper-slide  ">
-            <img src="{{ asset('assets/charizard.jpg') }}" alt="Slide 1"
+            <img src="{{ asset('assets/ayamchicken.jpg') }}" alt="Slide 1"
                 class="w-36 h-52 md:w-40 md:h-60 lg:w-72 lg:h-80 rounded-3xl object-cover">
         </div>
         <div class="swiper-slide  ">
-            <img src="{{ asset('assets/charizard.jpg') }}" alt="Slide 1"
+            <img src="{{ asset('assets/ayamgoreng.jpg') }}" alt="Slide 1"
+                class="w-40 h-52 md:w-52 md:h-60 lg:w-72 lg:h-80 rounded-3xl object-cover">
+        </div>
+
+        <div class="swiper-slide  ">
+            <img src="{{ asset('assets/ayamgoreng2.jpg') }}" alt="Slide 1"
                 class="w-36 h-52 md:w-40 md:h-60 lg:w-72 lg:h-80 rounded-3xl object-cover">
         </div>
         <div class="swiper-slide  ">
-            <img src="{{ asset('assets/charizard.jpg') }}" alt="Slide 1"
-                class="w-36 h-52 md:w-40 md:h-60 lg:w-72 lg:h-80 rounded-3xl object-cover">
-        </div>
-        <div class="swiper-slide  ">
-            <img src="{{ asset('assets/charizard.jpg') }}" alt="Slide 1"
-                class="w-36 h-52 md:w-40 md:h-60 lg:w-72 lg:h-80 rounded-3xl object-cover">
-        </div>
-        <div class="swiper-slide  ">
-            <img src="{{ asset('assets/charizard.jpg') }}" alt="Slide 1"
+            <img src="{{ asset('assets/ayamchicken.jpg') }}" alt="Slide 1"
                 class="w-36 h-52 md:w-40 md:h-60 lg:w-72 lg:h-80 rounded-3xl object-cover">
         </div>
 
