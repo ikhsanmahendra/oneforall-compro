510a5a993ef29dff58d2e998a5817d6ff340b0c5
Ikhsan <ikhsanmahendra14@gmail.com>
2024-12-22 15:52:30 +0700

    fix: sliders

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -48,7 +48,7 @@
     <div class="swiper-wrapper">
         @foreach ($sliders as $slider)
             <div class="swiper-slide">
-                <img src="{{ asset('storage/' . $slider->image_path) }}" alt=""
+                <img src="{{ asset('image/' . $slider->image) }}" alt=""
                     class="w-40 h-52 md:w-52 md:h-60 lg:w-72 lg:h-80 rounded-3xl object-cover">
             </div>
         @endforeach
