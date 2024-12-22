567ab6de63155be4d3afbc49535e650e5d2d64cd
Ikhsan <ikhsanmahendra14@gmail.com>
2024-12-22 18:17:26 +0700

    fix: admin panel side bar

--- resources/views/pages/admin/partials/navbar.blade.php
+++ resources/views/pages/admin/partials/navbar.blade.php
@@ -1,8 +1,8 @@
-<nav class="sticky top-0 w-full h-16 flex items-center justify-between bg-red-600 px-8">
+<nav class="sticky top-0 w-full h-16 flex items-center justify-between bg-red-600 px-8 shadow-md">
   <div class="flex items-center">
     <a href="" class="text-2xl font-bold text-white">One For All Chicken</a>
   </div>
   <div class="flex items-center">
-    <a href="/logout" class="text-white text-sm w-fit h-fit px-3 py-2 bg-red-500 rounded-lg">Logout</a>
+    <a href="/logout" class="text-white text-lg w-fit h-fit px-3 py-1 bg-black rounded-full font-semibold">Logout</a>
   </div>
 </nav>
