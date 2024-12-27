d70a5a235d5e66affe10a76f880e0d24275823d2
MuhamadRidwanPermana <a2.2000068@mhs.stmik-sumedang.ac.id>
2024-12-27 21:43:59 +0700

    feat: add crud page menu

--- resources/views/pages/admin/partials/sidebar.blade.php
+++ resources/views/pages/admin/partials/sidebar.blade.php
@@ -2,6 +2,7 @@
   <div class="w-full h-16 grid gap-5 items-center justify-center my-5">
     <a href="/dashboard" class="flex justify-center w-full h-fit px-5 py-2 bg-white font-semibold font-sans text-primary-500 rounded-lg hover:bg-gray-200 active:bg-gray-300 focus:outline-none focus:ring focus:ring-gray-300">Dashboard</a>
     <a href="/sliders" class="flex justify-center w-full h-fit px-5 py-2 bg-white font-semibold font-sans text-primary-500 rounded-lg hover:bg-gray-200 active:bg-gray-300 focus:outline-none focus:ring focus:ring-gray-300">Slider Management</a>
+    <a href="/menus" class="flex justify-center w-full h-fit px-5 py-2 bg-white font-semibold font-sans text-primary-500 rounded-lg hover:bg-gray-200 active:bg-gray-300 focus:outline-none focus:ring focus:ring-gray-300">Menu Management</a>
     <a href="/galeries" class="flex justify-center w-full h-fit px-5 py-2 bg-white font-semibold font-sans text-primary-500 rounded-lg hover:bg-gray-200 active:bg-gray-300 focus:outline-none focus:ring focus:ring-gray-300">Galeri Management</a>
     <a href="/blogs" class="flex justify-center w-full h-fit px-5 py-2 bg-white font-semibold font-sans text-primary-500 rounded-lg hover:bg-gray-200 active:bg-gray-300 focus:outline-none focus:ring focus:ring-gray-300">Blog Management</a>
   </div>
